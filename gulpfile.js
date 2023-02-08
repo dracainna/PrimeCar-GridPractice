27262049c4865a278d7f847ca13f13b1e2b2f203
dracainna <annasafonova.info@gmail.com>
2023-02-08 08:55:21 +0300

    init: start grid-project

--- gulpfile.js
+++ gulpfile.js
@@ -35,6 +35,7 @@ gulp.task('watch', function() {
     gulp.watch("src/fonts/**/*").on('all', gulp.parallel('fonts'));
     gulp.watch("src/icons/**/*").on('all', gulp.parallel('icons'));
     gulp.watch("src/img/**/*").on('all', gulp.parallel('images'));
+    gulp.watch("src/favicons/**/*").on('all', gulp.parallel('favicons'));
 });
 
 gulp.task('html', function () {
@@ -68,4 +69,10 @@ gulp.task('images', function () {
         .pipe(browserSync.stream());
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'html', 'images'));
+gulp.task('favicons', function () {
+    return gulp.src("src/favicons/**/*")
+        .pipe(gulp.dest("dist/favicons"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'html', 'images', 'favicons'));
